326bf7c352121edd2ec933265aa78b04eaacc62d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-02-15 23:09:52 +1100

    Astro 5.3, Tailwind 4 (#6)
    * Run astro upgrade
    
    * Update tailwind
    
    * Update package.json
    
    * Update src/components/Layout.astro (+1 more changes)
    
    * Update vite.config.js
    
    * Upgrade everything
    
    * Update tsconfig.json
    
    * Update astro.config.mjs (+2 more changes)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  vite: { plugins: [tailwindcss()] },
+  integrations: [react()]
 })
--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,3 +1,3 @@
 export function Example() {
-  return <div className="p-4">Hello!</div>
+  return <div className="p-4 text-gray-700">Hello!</div>
 }
