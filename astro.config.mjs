96490a44b5deaae71e5e9000c32d9ec83e201a9d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-02-15 22:26:06 +1100

    Update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwindcss(), react()]
 })
--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,3 +1,3 @@
 export function Example() {
-  return <div className="p-4">Hello!</div>
+  return <div className="p-4 text-green-400">Hello!</div>
 }
