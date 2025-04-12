6b2c4d6f3c39ff5a2a1435286c7374215f3dd32f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2025-04-12 22:43:22 +1000

    build: add simple-import-sort eslint plugin and update scripts for checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,15 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginAstro from 'eslint-plugin-astro'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort
+    },
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
